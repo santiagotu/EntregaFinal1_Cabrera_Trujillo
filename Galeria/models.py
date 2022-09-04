a7f205bb1f2ab5ccfd06e4d3f3a18192c090c81e
Sabrina.Cabrera <ytanirbas7@gmail.com>
2022-09-04 10:18:55 -0300

    modelos con fk

--- Galeria/models.py
+++ Galeria/models.py
@@ -4,11 +4,22 @@ class Artista(models.Model):
     nombre = models.CharField(max_length=60)
     estilo = models.CharField(max_length=60)
 
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Estilo: {self.estilo}"
+
 class Obra(models.Model):
     nombre = models.CharField(max_length=60)
     fecha = models.DateField()
     precio = models.FloatField()
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Fecha: {self.estilo} - Precio: {self.precio}"    
 
 class Avaluador(models.Model):
     nombre = models.CharField(max_length=60)
     fecha = models.DateField()
+    obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Fecha: {self.fecha}"
