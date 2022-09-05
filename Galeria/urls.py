2c9d417837c606932b07a7da89ff59494f480ff8
Sabrina.Cabrera <ytanirbas7@gmail.com>
2022-09-05 18:44:13 -0300

    settings y index

--- Galeria/urls.py
+++ Galeria/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from Galeria import views
 
+app_name = "Galeria"
 urlpatterns = [
     path('admin', admin.site.urls), 
-    path('', views.inicio, name="Inicio"),
+    path('', views.inicio, name='inicio'),
     path('artistas', views.artistas, name="artista"),
     path('avaluadores', views.avaluadores, name="avaluador"),
     path('obras', views.obras, name="obra"),
