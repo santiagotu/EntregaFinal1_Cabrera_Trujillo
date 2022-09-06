ac0e9f994014d698b5e84ef59c666d3da1cb3631
Sabrina.Cabrera <ytanirbas7@gmail.com>
2022-09-05 23:33:07 -0300

    modificacion de htmls

--- Final_Sabrina_y_Santiago/urls.py
+++ Final_Sabrina_y_Santiago/urls.py
@@ -1,8 +1,11 @@
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     #path('inicio/', admin.site.urls),
+    path('', lambda req: redirect('inicio')),
     path('Galeria/', include('Galeria.urls')),
+
 ]
 
